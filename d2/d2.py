8b50f2d54798e6a1f09e8dc3ec9f70c84de0aec9
theabm <andres.bermeo-marinelli@inria.fr>
2024-12-03 09:50:10 +0100

    finished d2

--- d2/d2.py
+++ d2/d2.py
@@ -1,5 +1,9 @@
 # part 1
 
+def process_report(report):
+    return all([(pair2>pair1 and pair2-pair1<4) for pair1,pair2 in zip(report, report[1:])]) or all((pair2<pair1 and pair1-pair2<4) for pair1,pair2 in zip(report, report[1:]))
+
+unsafe_reports = []
 with open('d2/input.txt', 'r') as file:
     safe = 0
     for line in file:
@@ -8,20 +12,20 @@ with open('d2/input.txt', 'r') as file:
         decreasing = all((pair2<pair1 and pair1-pair2<4) for pair1,pair2 in zip(report, report[1:]))
         if increasing or decreasing:
             safe += 1
+        else:
+            unsafe_reports.append(report)
 print(safe)
 
-# part 2
-with open('d2/input.txt', 'r') as file:
-    safe = 0
-    for line in file:
-        report = [int(elem) for elem in line.split()]
-        lenm1 = len(report)-1
-        increasing = sum([(pair2>pair1 and pair2-pair1<4) for pair1,pair2 in zip(report, report[1:])])
-        decreasing = sum((pair2<pair1 and pair1-pair2<4) for pair1,pair2 in zip(report, report[1:]))
-        if ((lenm1-increasing)<2 and decreasing == 0) or ((lenm1-decreasing)<2 and increasing == 0):
-            print(lenm1, increasing, decreasing)
-            safe += 1
-    
-print(safe)
+new_safe = 0
+for report in unsafe_reports:
+    for i in range(len(report)):
+        new_report = report[:i] + report[i+1:]
+        if process_report(new_report):
+            new_safe += 1
+            break
+print(new_safe)
+print(new_safe+safe)
+
+
 
 
